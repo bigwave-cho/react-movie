178bb65957fc21cc1649b38a317775ddb945c535
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-10 09:06:54 +0900

    initial settings

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//전역적으로 css 적용하는 방법.
+import './styles.css';
 
+// 모든 컴포넌트가 최종적으로 여기서 모이고
+// public의 index.html 파일을 기준으로 렌더링을 실시함.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
